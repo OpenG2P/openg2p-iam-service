4be0f48cbdcab44eb6954ff9340369d2fa1cf637
vin0dkhichar <abbb61667@gmail.com>
2025-11-19 13:21:48 +0530

    Remove the relative url and add absolute url

--- src/openg2p_portal_server/schemas/department.py
+++ src/openg2p_portal_server/schemas/department.py
@@ -4,12 +4,11 @@ from pydantic import BaseModel
 class DepartmentResponse(BaseModel):
     department_name: str
     department_mnemonic: str
-    base_url: str
-    bridge_relative_url: str
-    spar_relative_url: str
-    pbms_relative_url: str
-    registry_relative_url: str
-    superset_relative_url: str
+    bridge_url: str
+    spar_url: str
+    pbms_url: str
+    registry_url: str
+    superset_url: str
 
     class Config:
         from_attributes = True
