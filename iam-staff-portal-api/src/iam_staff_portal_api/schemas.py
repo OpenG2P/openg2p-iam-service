9115f35cfd47358333d4665bdb9ee18d4f53fea3
vin0dkhichar <abbb61667@gmail.com>
2026-03-20 12:29:06 +0530

    G2P-4378  Update StaffPortalApplicationResponse schema to include 'disabled' field

--- iam-staff-portal-api/src/iam_staff_portal_api/schemas.py
+++ iam-staff-portal-api/src/iam_staff_portal_api/schemas.py
@@ -5,10 +5,12 @@ class StaffPortalApplicationResponse(BaseModel):
     id: int
     application_mnemonic: str
     application_description: Optional[str] = None
+    application_url: Optional[str] = None
     icon_base64: Optional[str] = None
     width: Optional[int] = None
     application_url: Optional[str] = None
     order: Optional[int] = None
+    disabled: bool
 
 class ApplicationActionResponse(BaseModel):
     application_id: int
