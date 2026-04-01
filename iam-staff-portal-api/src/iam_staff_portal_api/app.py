b717c246caafab4c6f649fa9e1e1e1cdb2273830
tahzeer <tahzeer.ash@gmail.com>
2026-04-01 14:47:08 +0530

    feat(data): add initial staff roles JSON data

--- iam-staff-portal-api/src/iam_staff_portal_api/app.py
+++ iam-staff-portal-api/src/iam_staff_portal_api/app.py
@@ -25,6 +25,7 @@ from .models import (
     StaffRole,
     StaffRolePermission,
 )
+from .data import run_data_loader
 
 
 class Initializer(AuthInitializer):
@@ -47,5 +48,7 @@ class Initializer(AuthInitializer):
             await StaffRole.create_migrate()
             await StaffApplicationPermission.create_migrate()
             await StaffRolePermission.create_migrate()
+
+            await run_data_loader()
 
         asyncio.run(migrate())
