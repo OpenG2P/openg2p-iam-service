76251714bd0a0c3d193a91f95a8d882d2ceca7a2
tahzeer <tahzeer.ash@gmail.com>
2026-04-03 11:58:26 +0530

    refactor(data): update loader to use config vars for application irls

--- iam-staff-portal-api/src/iam_staff_portal_api/config.py
+++ iam-staff-portal-api/src/iam_staff_portal_api/config.py
@@ -1,6 +1,7 @@
 from iam_core.user_auth.config import Settings as BaseSettings
 from iam_core.user_auth.config import ApiAuthSettings
 
+from pydantic import Field
 from pydantic_settings import SettingsConfigDict
 
 
@@ -12,5 +13,13 @@ class Settings(BaseSettings):
         env_nested_delimiter="__",
     )
     auth_api_get_staff_portal_applications: ApiAuthSettings = ApiAuthSettings(enabled=True)
+    data_application_urls: dict[str, str] = Field(
+        default_factory=lambda: {
+            "rancher_application_url": "https://rancher.openg2p.org",
+            "keycloak_application_url": "https://keycloak.openg2p.org",
+            "registry_application_url": "https://registry.openg2p.org",
+            "minio_application_url": "https://minio.openg2p.org",
+        }
+    )
     cache_expire_seconds: int = 7*24*60*60      # 7 days
     data_dir: str = "/opt/iam-staff-portal-data"
